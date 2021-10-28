e1c0b51a200919c772c5ecc81b322f554a68c672
Fernao Shiotsuki <fernaospalma@gmail.com>
2021-10-28 17:07:08 -0300

    Refatorando codigo, removendo classes e renomeando props

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import "./App.css";
 import DevDisplay from "./components/Devs/Developer";
 import "./components/Devs/index.css";
 function App() {
-  class Devs {
-    constructor(name, age, country) {
-      this._name = name;
-      this._age = age;
-      this._country = country;
-    }
-  }
-  const dev1 = new Devs("Gabriel", 19, "Brazil");
-  const dev2 = new Devs("Filipe", 28, "Brazil");
-  const dev3 = new Devs("Mariana", 24, "Italia");
+  const Devs = [
+    { name: "Gabriel", age: 19, country: "Brazil" },
+    { name: "Filipe", age: 28, country: "Brazil" },
+    { name: "Mariana", age: 24, country: "Italia" },
+  ];
 
   return (
     <div className="App">
-      <DevDisplay name={dev1._name} age={dev1._age} country={dev1._country} />
-      <DevDisplay name={dev2._name} age={dev2._age} country={dev2._country} />
-      <DevDisplay name={dev3._name} age={dev3._age} country={dev3._country} />
+      <DevDisplay
+        name={Devs[0].name}
+        age={Devs[0].age}
+        country={Devs[0].country}
+      />
+      <DevDisplay
+        name={Devs[1].name}
+        age={Devs[1].age}
+        country={Devs[1].country}
+      />
+      <DevDisplay
+        name={Devs[2].name}
+        age={Devs[2].age}
+        country={Devs[2].country}
+      />
     </div>
   );
 }
--- src/components/Devs/Developer.jsx
+++ src/components/Devs/Developer.jsx
@@ -1,9 +1,9 @@
-function DevDisplay(x) {
+function DevDisplay(props) {
   return (
     <div>
-      <h2> Dev: {x.name}</h2>
-      <h3>Idade: {x.age}</h3>
-      <h3>Pais: {x.country}</h3>
+      <h2> Dev: {props.name}</h2>
+      <h3>Idade: {props.age}</h3>
+      <h3>Pais: {props.country}</h3>
     </div>
   );
 }
